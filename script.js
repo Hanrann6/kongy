c67ab96b9932029b4ba700a34a92adee77402c83
Hanrann6 <yookrann@ewhain.net>
2023-03-17 22:55:07 +0900

    js알람 추가

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",
+ function(e){
+    let configID = document.querySelector("#id button")
+
+    configID.addEventListener("click",
+     function(e){
+        alert("닉네임이 변경되었습니다.")
+     }
+    )
+ }
+)
+
